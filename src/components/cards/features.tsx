135d64a7d0ed91a14b4a27d6a65b8b367ea1a3be
Samplayz678 <94945980+Samplayz678@users.noreply.github.com>
2025-02-06 17:44:27 +0530

    Update features.tsx

--- src/components/cards/features.tsx
+++ src/components/cards/features.tsx
@@ -6,15 +6,15 @@ const Features: React.FC = () => {
       <div className="container mx-auto">
         <h2 className="text-3xl font-bold mb-4">Command List</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {/* Music Commands */}
+          {/* General Commands */}
           <div className="bg-gray-900 p-6 rounded-lg shadow-md hover:shadow-lg transform hover:-translate-y-1 transition duration-300 cursor-pointer">
             <h3 className="text-xl font-bold mb-2">General Commands</h3>
             <p className="text-lg text-gray-300">
-              generate, help, invite, ping, profile, redeem
+             help, invite, ping, premium, profile, subscribe
             </p>
           </div>
 
-          {/* Filter Commands */}
+          {/* Music Commands */}
           <div className="bg-gray-900 p-6 rounded-lg shadow-md hover:shadow-lg transform hover:-translate-y-1 transition duration-300 cursor-pointer">
             <h3 className="text-xl font-bold mb-2">Music Commands</h3>
             <p className="text-lg text-gray-300">
@@ -22,18 +22,18 @@ const Features: React.FC = () => {
             </p>
           </div>
 
-          {/* Information Commands */}
+          {/* Settings Commands */}
           <div className="bg-gray-900 p-6 rounded-lg shadow-md hover:shadow-lg transform hover:-translate-y-1 transition duration-300 cursor-pointer">
             <h3 className="text-xl font-bold mb-2">Setting Commands</h3>
             <p className="text-lg text-gray-300">
              247, autoplay
             </p>
           </div>
-          {/* Filter Commands */}
+          {/* Playlist Commands */}
           <div className="bg-gray-900 p-6 rounded-lg shadow-md hover:shadow-lg transform hover:-translate-y-1 transition duration-300 cursor-pointer">
             <h3 className="text-xl font-bold mb-2">Music Commands</h3>
             <p className="text-lg text-gray-300">
-              clear, filter, join, leave, loop, lyric, pause, play, previous, queue, remove, resume, seek, shuffle, skip, stop, volume
+             playlist-add, playlist-create, playlist-delete, playlist-list, playlist-load, playlist-remove
             </p>
           </div>
         </div>
